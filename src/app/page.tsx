fdbb1ccf3c8d33f841a3bc641ae8d8830b2f6831
sincerelyyyash <yta0088@gmail.com>
2024-05-12 03:20:38 +0530

    Course card section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CoursesCard from "@/components/CoursesCard";
 import HeroSection from "@/components/HeroSection";
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
+    <CoursesCard/>
     </main>
   );
 }
